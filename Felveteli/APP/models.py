c84096b27a580ae56022a4c2840f3b8d52992d6f
Farkas Ükös <ukosfarkas@gmail.com>
2022-03-25 13:01:03 +0100

    A pénteki óra eredménye...

--- Felveteli/APP/models.py
+++ Felveteli/APP/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Diak(models.Model):
+    azonosito = models.IntegerField(max_length = 11)
+    nev = models.CharField(max_length = 256)
+    szak = models.CharField(max_length = 256)
+    pont = models.IntegerField(max_length=3)
+    megfelelt = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Diák'
+        verbose_name_plural = 'Diákok'
+
+    def __str__(self):
+        """Unicode representation of Kerdes."""
+        return f"A szóban forgó diák neve {self.nev}"
--- Felveteli/APP/views.py
+++ Felveteli/APP/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Diak
 
-# Create your views here.
+def index(request):
+    if request.method=="POST":
+        sito = request.POST['azonosito']
+    
+    template = "index.html"
+    hanincs = "nincs.html"
+
+    alany = Diak.objects.filter(azonosito = sito).first()
+
+    if alany != None:
+        context={'Feri': alany}
+        return render(request, template, context)
+    else:
+        return render(request, hanincs)
+    
+    
