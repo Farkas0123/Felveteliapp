062930009681355f67e18acc941c79749389dbad
Farkas Ükös <ukosfarkas@gmail.com>
2022-04-12 11:03:10 +0200

    commit

--- APP/views.py
+++ APP/views.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from posixpath import split
 from re import template
 from tkinter import Variable
@@ -28,12 +29,18 @@ def feltoltes(request):
     template = "upload.html"
     if request.method =="POST" and request.POST['jelszo']=="jelszo":
         tabla = request.POST['adatok'].split("\t")
+        i = 0
         for diak in tabla:
             akt = diak.split(",")
-            d = Diak.objects.filter(azonosito=akt[0]).first()
-            if d == None:
-                Diak.objects.create(azonosito=akt[0],nev=akt[1],szak=akt[2],pont=akt[3],megfelelt=akt[4],)
-            elif Diak.objects.filter(azonosito=akt[0]).first().szak != akt[2]:
-                Diak.objects.create(azonosito=akt[0],nev=akt[1],szak=akt[2],pont=akt[3],megfelelt=akt[4],)
+            if akt[0] is not int or akt[1] is not str or akt[2] is not str or akt[3] is not int or akt[4] is not bool and not akt[0] == "":
+                return render(request, template, {'hiba': f"Valamelyik adat hibás a(z) {i+1} sorban"})
+            else:
+                d = Diak.objects.filter(azonosito=akt[0]).first()
+                if d == None:
+                    Diak.objects.create(azonosito=akt[0],nev=akt[1],szak=akt[2],pont=akt[3],megfelelt=akt[4],)
+                elif Diak.objects.filter(azonosito=akt[0]).first().szak != akt[2]:
+                    Diak.objects.create(azonosito=akt[0],nev=akt[1],szak=akt[2],pont=akt[3],megfelelt=akt[4],)
+                i =+ 1
+        return render(request, template, {})
                 
     return render(request, template, {})
